fd9767d25e5896cd4a32a53724fc1e643062bd28
Faiaz-Rahman <faiazrahman70@gmail.com>
2025-07-24 22:31:58 +0600

    updated routes for handling notification ...

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express'
 import { createItem } from '../controllers/testController'
+import { notifyUser } from '../controllers/notification'
 
 const router = Router()
 
 router.post('/', createItem)
+router.get('/notify', notifyUser)
 
 export default router
