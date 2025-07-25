740f42f4d32920e2f2373c9d688cbf14c45033c4
Faiaz-Rahman <faiazrahman70@gmail.com>
2025-07-25 07:49:22 +0600

    removed the cert method, because it involves uploading the credentials.json to render() ...

--- src/utils/firebase-admin.ts
+++ src/utils/firebase-admin.ts
@@ -2,8 +2,13 @@ import * as admin from 'firebase-admin'
 
 const serviceAccount = require('./yeamazing-partner-firebase-adminsdk.json')
 
+// export const AdminSdk = admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount),
+// })
+
 export const AdminSdk = admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  projectId: process.env.PROJECT_ID,
+  serviceAccountId: process.env.CLIENT_EMAIL,
 })
 
 export { admin }
