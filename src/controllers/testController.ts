57429ee286cc9b2f39f57f8959281bbf7dc7d57a
Faiaz-Rahman <faiazrahman70@gmail.com>
2025-07-25 14:37:44 +0600

    minor changes, updated socket response ...

--- src/controllers/testController.ts
+++ src/controllers/testController.ts
@@ -1,12 +1,12 @@
 import { Request, Response, NextFunction } from 'express'
 
-// Create an item
+// Create an item (demo)
 export const createItem = (req: Request, res: Response, next: NextFunction) => {
-	try {
-		res.status(201).json({
-			message: 'created',
-		})
-	} catch (error) {
-		next(error)
-	}
+  try {
+    res.status(201).json({
+      message: 'created',
+    })
+  } catch (error) {
+    next(error)
+  }
 }
--- src/socket/socketHandler.ts
+++ src/socket/socketHandler.ts
@@ -1,29 +1,48 @@
 import { Server, Socket } from 'socket.io'
 
-const onlineUsers = new Map<string, string>() // userId -> socketId
+interface userData {
+  userId: string
+  username: string
+  imageUrl: string | null
+  email: string
+}
+
+interface onlineUsersData extends userData {
+  socketId: string
+}
+
+const onlineUsers = new Map<string, onlineUsersData>() // userId -> onlineUsersData
 
 export default function socketHandler(io: Server) {
   io.on('connection', (socket: Socket) => {
     console.log(`New socket connected: ${socket.id}`)
 
-    socket.on('user-online', (userId: string) => {
-      onlineUsers.set(userId, socket.id)
-      console.log('emitting online users =>', onlineUsers)
-      io.emit('update-user-status', Array.from(onlineUsers.keys()))
+    socket.on('user-online', (userData: userData) => {
+      onlineUsers.set(userData.userId, {
+        socketId: socket.id,
+        imageUrl: userData.imageUrl,
+        userId: userData.userId,
+        username: userData.username,
+        email: userData.email,
+      } as onlineUsersData)
+
+      console.log('==> emitting online users =>', onlineUsers)
+
+      io.emit('update-user-status', Array.from(onlineUsers.values()))
     })
 
     socket.on('disconnect', () => {
-      for (const [userId, sockId] of onlineUsers.entries()) {
-        if (sockId === socket.id) {
+      for (const [userId, userdata] of onlineUsers.entries()) {
+        if (userdata.socketId === socket.id) {
           onlineUsers.delete(userId)
           break
         }
       }
-      io.emit('update-user-status', Array.from(onlineUsers.keys()))
+      io.emit('update-user-status', Array.from(onlineUsers.values()))
     })
 
     socket.on('connect_error', (err) => {
-      console.error('❌ Socket connection error:', err)
+      console.error('==> Socket connection error:', err)
     })
   })
 }
