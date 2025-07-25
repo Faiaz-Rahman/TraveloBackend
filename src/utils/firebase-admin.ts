d8195e070002f4258c4055e6ee2ae9ea7b1d9bfd
Faiaz-Rahman <faiazrahman70@gmail.com>
2025-07-25 15:03:42 +0600

    removed firebase-admin json, not reading credentials from it

--- src/utils/firebase-admin.ts
+++ src/utils/firebase-admin.ts
@@ -1,7 +1,5 @@
 import * as admin from 'firebase-admin'
 
-const serviceAccount = require('./yeamazing-partner-firebase-adminsdk.json')
-
 // export const AdminSdk = admin.initializeApp({
 //   credential: admin.credential.cert(serviceAccount),
 // })
