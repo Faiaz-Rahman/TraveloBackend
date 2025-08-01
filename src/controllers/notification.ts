2084035e95fe4433c20d83508243bccf3dd95292
Faiaz-Rahman <faiazrahman70@gmail.com>
2025-08-02 04:40:08 +0600

    admin credentials undefined issues resolved ...

--- src/controllers/notification.ts
+++ src/controllers/notification.ts
@@ -1,10 +1,14 @@
-import { Request, Response } from 'express'
+import { NextFunction, Request, Response } from 'express'
 import { admin } from '../utils/firebase-admin'
 
-export const notifyUser = async (req: Request, res: Response) => {
+export const notifyUser = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
   try {
     const userId = req.query.userid as string
-    console.log('the user id got in the request is =>', userId)
+    console.log('user-id from query params =>', userId)
 
     if (!userId) {
       res.status(400).json({ message: 'userid is missing in query parameter' })
@@ -18,37 +22,39 @@ export const notifyUser = async (req: Request, res: Response) => {
 
     if (!snapshot.exists) {
       res.status(401).json({
-        message: 'Unauthorized! user does not exist ...',
+        message: 'unauthorized! user does not exist ...',
+      })
+    } else {
+      const userData = snapshot.data()
+
+      const targetFcm = userData?.fcm
+      const username = userData?.name
+
+      await admin.messaging().send({
+        token: targetFcm,
+        data: {
+          notifee: JSON.stringify({
+            title: 'Yeamazing',
+            body: `Received a new message from ${username}`,
+            android: {
+              channelId: 'default',
+              actions: [
+                {
+                  title: 'Mark as read',
+                  pressAction: {
+                    id: 'read',
+                  },
+                },
+              ],
+            },
+          }),
+        },
+      })
+
+      res.status(200).json({
+        message: `notification was sent to ${userId}`,
       })
     }
-
-    const targetFcm = snapshot.data()?.fcm
-    const username = snapshot.data()?.name
-
-    await admin.messaging().send({
-      token: targetFcm,
-      data: {
-        notifee: JSON.stringify({
-          title: 'Yeamazing',
-          body: `Received a new message from ${username}`,
-          android: {
-            channelId: 'default',
-            actions: [
-              {
-                title: 'Mark as read',
-                pressAction: {
-                  id: 'read',
-                },
-              },
-            ],
-          },
-        }),
-      },
-    })
-
-    res.status(200).json({
-      message: `notification sent to ${userId}`,
-    })
   } catch (error) {
     res.status(500).json({ message: 'Internal Server Error!!!' })
   }
--- src/utils/firebase-admin.ts
+++ src/utils/firebase-admin.ts
@@ -1,12 +1,11 @@
 import * as admin from 'firebase-admin'
 
-// export const AdminSdk = admin.initializeApp({
-//   credential: admin.credential.cert(serviceAccount),
-// })
-
 export const AdminSdk = admin.initializeApp({
-  projectId: process.env.PROJECT_ID,
-  serviceAccountId: process.env.CLIENT_EMAIL,
+  credential: admin.credential.cert({
+    clientEmail: process.env.CLIENT_EMAIL!,
+    privateKey: process.env.PRIVATE_KEY!,
+    projectId: process.env.PROJECT_ID!,
+  }),
 })
 
 export { admin }
