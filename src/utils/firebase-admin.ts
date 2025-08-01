ff2800f5b112450e928284116565184731e2b750
Faiaz-Rahman <faiazrahman70@gmail.com>
2025-08-02 05:41:04 +0600

    replacing escaped newline with actual newline ...

--- src/utils/firebase-admin.ts
+++ src/utils/firebase-admin.ts
@@ -3,8 +3,8 @@ import * as admin from 'firebase-admin'
 export const AdminSdk = admin.initializeApp({
   credential: admin.credential.cert({
     clientEmail: process.env.CLIENT_EMAIL!,
-    privateKey: process.env.PRIVATE_KEY!,
     projectId: process.env.PROJECT_ID!,
+    privateKey: process.env.PRIVATE_KEY?.replace(/\\n/g, '\n'),
   }),
 })
 
